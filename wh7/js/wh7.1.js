bb990f028a7e38cf5c41e650b4ba5235d019f467
Sergii Pogrebniy <pogrebniy89@gmail.com>
2020-05-24 15:59:51 +0300

    new test solution

--- wh7/js/wh7.1.js
+++ wh7/js/wh7.1.js
@@ -12,103 +12,146 @@
 // значения "n" счетчика, где n > 0, и n <= size, в случае установки
 // значения n которое не прошло валидацию выдать в консоль ошибку.
 
-function initSlider(size) {
-    let counter = 0;
+let slider;
 
-    if (counter >= 0 && counter <= size) {
-        return counter;
-    } else if (counter < 0 || counter >= size) {
+slider = {
+    counter: 0,
+    up() {
+        this.counter++;
+        return this
+    },
+    down() {
+        this.counter--;
+        return this
+    },
+    set(value) {
+        this.counter = value;
+        return this
+    },
+    initSlider(size) {
+    if (this.counter >= 0 && this.counter <= size) {
+        return this.counter;
+    } else if (this.counter < 0 || this.counter >= size) {
         console.log('значение за предалами диапазона');
-        counter = 0;
-        return counter;
+        this.counter = 0;
+        return this.counter;
 
     }
-
 }
-
-    let obj = {
-
-        up: function () {
-            this.counter++;
-            return this.counter;
-        },
-        down: function () {
-            this.counter--;
-            return this.counter;
-        },
-        set: function (value) {
-            this.counter = value;
-            return this.counter;
-        }
-    };
+};
 
 
 
-let count = new initSlider(5);
+// let count = slider.initSlider(5);
+
+console.log(slider.up().initSlider(5));
+console.log(slider.up().initSlider(5));
+console.log(slider.up().initSlider(5));
+console.log(slider.up().initSlider(5));
+console.log(slider.up().initSlider(5));
+console.log(slider.up().initSlider(5));
+console.log(slider.up().initSlider(5));
+console.log(slider.up().initSlider(5));
 
 
-console.log(count.up());
-console.log(count.up());
-console.log(count.up());
-console.log(count.up());
-console.log(count.up());
-console.log(count.up());
-console.log(count.up());
-console.log(count.up());
-console.log(count.down());
-console.log(count.down());
-console.log(count.set(0));
-console.log(count.down());
-console.log(count.down());
-console.log(count.down());
-console.log(count.down());
-console.log(count.down());
-console.log(count.down());
-console.log(count.down());
-console.log(count.down());
-console.log(count.set(1));
-console.log(count.set(3));
-console.log(count.set(5));
-console.log(count.set(6));
-console.log(count.set(4));
-console.log(count.set(2));
-console.log(count.set(-5));
-console.log(count.set(2));
-console.log(count.set(8));
-console.log(count.set(1));
-console.log(count.set(33));
 
-let count1 = new initSlider(10);
-
-console.log(count1.up(),'счетчик 2');
-console.log(count1.up(),'счетчик 2');
-console.log(count1.up(),'счетчик 2');
-console.log(count1.up(),'счетчик 2');
-console.log(count1.up());
-console.log(count1.up());
-console.log(count1.up());
-console.log(count1.up());
-console.log(count1.up());
-console.log(count1.up());
-console.log(count1.down());
-console.log(count1.down());
-console.log(count1.set(0));
-console.log(count1.down());
-console.log(count1.down());
-console.log(count1.down());
-console.log(count1.down());
-console.log(count1.down());
-console.log(count1.down());
-console.log(count1.down());
-console.log(count1.down());
-console.log(count1.set(1));
-console.log(count1.set(3));
-console.log(count1.set(5));
-console.log(count1.set(6));
-console.log(count1.set(4));
-console.log(count1.set(2));
-console.log(count1.set(-5));
-console.log(count1.set(2));
-console.log(count1.set(8));
-console.log(count1.set(1));
-console.log(count1.set(33));
+// function initSlider(size) {
+//     let counter = 0;
+//
+//     if (counter >= 0 && counter <= size) {
+//         return counter;
+//     } else if (counter < 0 || counter >= size) {
+//         console.log('значение за предалами диапазона');
+//         counter = 0;
+//         return counter;
+//
+//     }
+//
+// }
+//
+//     let obj = {
+//
+//         up: function () {
+//             this.counter++;
+//             return this.counter;
+//         },
+//         down: function () {
+//             this.counter--;
+//             return this.counter;
+//         },
+//         set: function (value) {
+//             this.counter = value;
+//             return this.counter;
+//         }
+//     };
+//
+//
+//
+// let count = new initSlider(5);
+//
+//
+// console.log(count.up());
+// console.log(count.up());
+// console.log(count.up());
+// console.log(count.up());
+// console.log(count.up());
+// console.log(count.up());
+// console.log(count.up());
+// console.log(count.up());
+// console.log(count.down());
+// console.log(count.down());
+// console.log(count.set(0));
+// console.log(count.down());
+// console.log(count.down());
+// console.log(count.down());
+// console.log(count.down());
+// console.log(count.down());
+// console.log(count.down());
+// console.log(count.down());
+// console.log(count.down());
+// console.log(count.set(1));
+// console.log(count.set(3));
+// console.log(count.set(5));
+// console.log(count.set(6));
+// console.log(count.set(4));
+// console.log(count.set(2));
+// console.log(count.set(-5));
+// console.log(count.set(2));
+// console.log(count.set(8));
+// console.log(count.set(1));
+// console.log(count.set(33));
+//
+// let count1 = new initSlider(10);
+//
+// console.log(count1.up(),'счетчик 2');
+// console.log(count1.up(),'счетчик 2');
+// console.log(count1.up(),'счетчик 2');
+// console.log(count1.up(),'счетчик 2');
+// console.log(count1.up());
+// console.log(count1.up());
+// console.log(count1.up());
+// console.log(count1.up());
+// console.log(count1.up());
+// console.log(count1.up());
+// console.log(count1.down());
+// console.log(count1.down());
+// console.log(count1.set(0));
+// console.log(count1.down());
+// console.log(count1.down());
+// console.log(count1.down());
+// console.log(count1.down());
+// console.log(count1.down());
+// console.log(count1.down());
+// console.log(count1.down());
+// console.log(count1.down());
+// console.log(count1.set(1));
+// console.log(count1.set(3));
+// console.log(count1.set(5));
+// console.log(count1.set(6));
+// console.log(count1.set(4));
+// console.log(count1.set(2));
+// console.log(count1.set(-5));
+// console.log(count1.set(2));
+// console.log(count1.set(8));
+// console.log(count1.set(1));
+// console.log(count1.set(33));
