01b095f395f2f96edcfc90848179ee157f5c4ad0
Pogrebniy <pogrebniy89@gmail.com>
2020-06-04 16:23:22 +0300

    add setMarker, clear, separator

--- wh12/js/wh12.js
+++ wh12/js/wh12.js
@@ -10,32 +10,54 @@ function SuperArray(n, m, option) {
     }
 
 
-    // this.clear = function (direction, k) {
-    //
-    // }
-    //
+    this.clear = function (direction, k) {
+        if (direction === 'row') {
+            for (let j = 0, len = nArr[0].length; j < len; j++) {
+                nArr[k][j] = 0;
+            }
+
+            return nArr
+        }
+
+        if (direction === 'column') {
+            for (let i = 0, size = nArr.length; i < size; i++) {
+                nArr[i][k] = 0;
+            }
+            return nArr
+        }
+    };
+
     this.setMarker = function (marker) {
         nArr[marker.x][marker.y] = '&';
-        return console.log(nArr);
-    }
-    //
-    // this.goTo = function ({}) {
-    //
+        return nArr;
+    };
+
+    // this.goTo = function (goToMarker) {
     // }
     //
     // this.shift = function (direction) {
     //
     // }
-    return nArr;
+    // return nArr;
 }
 
 SuperArray.prototype.render = function (separator) {
+    let sep = [];
+    sep.length = 8;
+    for (let i = 0, size = sep.length; i < size; i++) {
+        sep[i] = separator;
+    }
 
-}
+    return console.log(sep.join(''))
+};
 
-let arr = new SuperArray(5,6,{min: 0, max: 100});
+let arr = new SuperArray(6, 10, {min: 0, max: 100});
 
-console.log(arr);
 
-arr.setMarker({x: 2, y: 3})
+console.log(arr.setMarker({x: 2, y: 3}));
 
+arr.render('+');
+
+
+arr.clear('row', 1);
+arr.clear('column', 1);
