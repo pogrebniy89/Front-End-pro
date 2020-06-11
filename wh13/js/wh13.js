62c4c4f4981c840e3e3291ba5bc393f3b9d1c4c1
Pogrebniy <pogrebniy89@gmail.com>
2020-06-11 14:15:07 +0300

    version 0.1

--- wh13/js/wh13.js
+++ wh13/js/wh13.js
@@ -1,1 +1,24 @@
-console.log('test');
+window.onload = function () {
+    let elements = document.querySelectorAll('form.decor > input');
+
+    let textA = document.getElementsByClassName('textAll');
+
+
+    setInterval(function () {
+            let test = '';
+
+            for (let i = 0, size = elements.length; i < size; i++) {
+                if (elements[i].value) {
+                    if (i === size - 1) {
+                        test += (elements[i].value + '.')
+                    } else {
+                        test += (elements[i].value + ', ');
+                    }
+                }
+            }
+            textA[0].value = test;
+        }
+
+        , 3000)
+
+};
