02ac525abcef02331d0806883217d6177cf3e4f1
Sergii Pogrebniy <pogrebniy89@gmail.com>
2020-06-21 18:08:30 +0300

    lecture 16 v0.1

--- wh16/js/main.js
+++ wh16/js/main.js
@@ -3,6 +3,17 @@ window.onload = function() {
 
     let button = document.querySelector('.button');
     let counter = document.querySelector('.counter');
+    let buttonWrap = document.getElementById('block');
+
+
+
+    buttonWrap.addEventListener('click',showClick);
+
+    function showClick(event) {
+        let target = event.target;
+        console.log(target.dataset.botton);
+    }
+
 
     let store = {
         setInStore: function (key, value) {
@@ -15,12 +26,13 @@ window.onload = function() {
 
     counter.innerHTML = store.getFromStore('counter') || 0;
 
-    var click = (function (counter) {
+    let click = (function (counter) {
         let counterValue = ++counter.innerHTML;
 
         store.setInStore('counter', counterValue);
 
     }).bind(null, counter);
+
 
 
     button.addEventListener('click', click);
