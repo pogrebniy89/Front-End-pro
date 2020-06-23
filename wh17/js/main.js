9d990efc75bc73a7849cb659b6e97255cd8c2ed3
Sergii Pogrebniy <pogrebniy89@gmail.com>
2020-06-23 23:12:40 +0300

    wh17 v0.1

--- wh17/js/main.js
+++ wh17/js/main.js
@@ -21,6 +21,7 @@ window.onload = function () {
         setSpan(i);
         buttons[i].addEventListener('click', click.bind(buttons[i], i));
         clear.addEventListener('click', clearAllCounter.bind(buttons[i], i));
+        set.addEventListener('click', setCounter.bind(setCounter[i], i))
     }
 
     function click(i) {
@@ -34,6 +35,23 @@ window.onload = function () {
         store.setInStore('counter-' + i, 0);
         setSpan(i);
     }
+
+    function setCounter(i) {
+        var count = prompt('установить счетчик', 'counter-1');
+        if(store.getFromStore('counter-' + i) === count){
+            console.log('nnnnn');
+        }
+
+
+        // if (!(store.getFromStore(count) === null)){
+        //    var val = +prompt('установить значение', '30');
+        //     store.setInStore(count, val);
+        //     getParent(buttons[i], '.block')
+        //         .querySelector('.counter').innerHTML = store.getFromStore(count);
+        // }
+    }
+
+
 
     function getParent(node, parentSelector) {
         return node.closest(parentSelector);
