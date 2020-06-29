ed69ba12fb11f44bef0eab23989b3cdef7d534f7
Sergii Pogrebniy <pogrebniy89@gmail.com>
2020-06-29 20:41:37 +0300

    wh15 v0.3

--- wh15/js/main.js
+++ wh15/js/main.js
@@ -94,25 +94,19 @@ window.onload = function () {
             .render('.container');
 
 
-        document.oncontextmenu = function(event) {
-            event.preventDefault();
-            alert("Контекстное меню документа");
-        };
-
     }
 
     menuModule();
 
     function contextMenu() {
 
-        document.oncontextmenu = function(event) {
+        document.oncontextmenu = function (event) {
             event.preventDefault();
             alert("Контекстное меню документа");
         };
 
         let data = {
             name: 'menuContext',
-            type: 'row', // row|column
             items: [
                 {
                     title: 'MenuRow',
@@ -124,6 +118,71 @@ window.onload = function () {
                 }
             ]
         };
+
+        let actions = {
+            MenuRow: function () {
+                console.log('Горизонтальное меню');
+                column()
+            },
+            MenuColumn: function () {
+                console.log('Вертикальное меню');
+
+            }
+        };
+
+        function MenuContext(data, actions) {
+            this.data = data;
+            this.actions = actions;
+            this.ul = null;
+        }
+
+        MenuContext.prototype.makeMenuItems = function () {
+            let fragment = document.createDocumentFragment();
+            let items = this.data.items;
+
+            for (let i = 0; i < items.length; i++) {
+                let li = document.createElement('li');
+                let item = items[i];
+                li.classList.add('context-menu');
+                li.innerText = item.title;
+                li.addEventListener('click', actions[item.handler]);
+                fragment.append(li);
+            }
+
+            return fragment;
+        };
+        MenuContext.prototype.makeMenuContainer = function () {
+            let ul = document.createElement('ul');
+            ul.classList.add(data.type);
+
+            return ul;
+        };
+
+        MenuContext.prototype.makeMenu = function () {
+            let ul = this.makeMenuContainer();
+            let fragment = this.makeMenuItems();
+            ul.append(fragment);
+            this.ul = ul;
+
+            return this;
+        };
+
+        MenuContext.prototype.render = function (selector) {
+            let parent = document.querySelector(selector);
+
+            if (this.ul && parent) {
+                document.querySelector(selector).append(this.ul);
+            }
+
+            return this;
+        };
+
+        let menu = new MenuContext(data, actions);
+
+        menu
+            .makeMenu()
+            .render('.container');
+
 
     }
 
