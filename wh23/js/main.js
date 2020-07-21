8c24d7404f78326ee8e198c267d5512769cfa44d
Sergii Pogrebniy <pogrebniy89@gmail.com>
2020-07-21 22:31:24 +0300

    wh23 v0.2

--- wh23/js/main.js
+++ wh23/js/main.js
@@ -1,7 +1,26 @@
 console.log(Vue, 'vue');
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Привет, Vue!'
+
+let data = {
+    listLeft: [
+        'Важное дело 1',
+        'Важное дело 2',
+        'Важное дело 3',
+        'Важное дело 4',
+        'Важное дело 5',
+    ],
+    listRight: [
+        'Дело 1',
+        'Дело 2',
+        'Дело 3',
+        'Дело 4',
+        'Дело 5',
+    ],
+    onClick(value) {
+        console.log(value, 'результа кнопки');
     }
+};
+
+const TaskManager = new Vue({
+    el: '#app',
+    data: data
 })
